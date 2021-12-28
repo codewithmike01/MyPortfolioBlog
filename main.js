a738bb5804ce14a01402ac1c2cc2c45992c2cf96
ginohmk <michotall95@gmail.com>
2021-12-28 11:29:47 +0100

    Fix Z-index issue on mobile menu when scrolling

--- main.js
+++ main.js
@@ -2,11 +2,17 @@ const hamMenu = document.querySelector('.ham-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const closeMenu = document.getElementById('nav-close');
 
+/*-----------------------------
+|          FUNCTIONS          |
+|                             |
+------------------------------*/
+
 function openCloseMenu() {
   console.log('hello');
   mobileMenu.classList.toggle('hide');
   hamMenu.classList.toggle('hide');
 }
 
+// Close & Open Mobile Menu
 hamMenu.addEventListener('click', openCloseMenu);
 closeMenu.addEventListener('click', openCloseMenu);
