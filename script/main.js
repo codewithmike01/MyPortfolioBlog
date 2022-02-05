18ac64d5048e819dbfd6cbefcf30b6f67407a612
ginohmk <michotall95@gmail.com>
2022-02-05 17:12:34 +0100

    Add dynamic skill and project rendering

--- script/main.js
+++ script/main.js
@@ -1,17 +1,3 @@
-const hamMenu = document.querySelector('.ham-menu');
-const mobileMenu = document.querySelector('.mobile-menu');
-const closeMenu = document.getElementById('nav-close');
-
-/*-----------------------------
-|          FUNCTIONS          |
-|                             |
-------------------------------*/
-
-function openCloseMenu() {
-  mobileMenu.classList.toggle('hide');
-  hamMenu.classList.toggle('hide');
-}
-
-// Close & Open Mobile Menu
-hamMenu.addEventListener('click', openCloseMenu);
-closeMenu.addEventListener('click', openCloseMenu);
+import './popUp.js';
+import './skillsRender.js';
+import './projectRender.js';
---
+++ script/skillsRender.js
@@ -0,0 +1,28 @@
+const skills = [
+  { name: 'GIT', imageUrl: './images/gitSkill.png" ' },
+  { name: 'GITHUB', imageUrl: './images/githubSkill.png" ' },
+  { name: 'HTML5', imageUrl: './images/html5Skill.png" ' },
+  { name: 'CSS3', imageUrl: './images/css3Skill.png" ' },
+  { name: 'JAVASCRIPT', imageUrl: './images/jsSkill.png" ' },
+  { name: 'JEST', imageUrl: './images/gitSkill.png" ' },
+  { name: 'WEBPACK', imageUrl: './images/jestSkill.png" ' },
+  { name: 'SASS', imageUrl: './images/sassSkill.png" ' },
+  { name: 'BOOTSTRAP', imageUrl: './images/bootstrapSkill.png" ' },
+  { name: 'PHOTOSHOP', imageUrl: './images/photoshopSkill.png" ' },
+];
+
+const skillsContainer = document.querySelector('.skills');
+
+skills.forEach((item) => {
+  let divSkill = document.createElement('div');
+  divSkill.classList.add('skill-details');
+  divSkill.innerHTML = ` 
+  <div class="skill-name">
+    <h4>${item.name}</h4>
+  </div>
+  <div class="imageSkill">
+    <img src="${item.imageUrl}" alt="" />
+  </div>`;
+
+  skillsContainer.appendChild(divSkill);
+});
