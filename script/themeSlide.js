08cb131343092fdad9b470e3c9ed85f8d7102604
ginohmk <michotall95@gmail.com>
2022-04-16 07:57:31 +0100

    Add persistent dark mode

--- script/themeSlide.js
+++ script/themeSlide.js
@@ -2,6 +2,13 @@ const gear = document.querySelector('.gear');
 let body_class = document.querySelector('body');
 let primary_color = gear.nextElementSibling;
 let secondary_color = primary_color.nextElementSibling;
+let store_theme =
+  localStorage.getItem('theme') || localStorage.setItem('theme', 'theme-1');
+body_class.classList = store_theme;
+
+function set_theme_store(theme) {
+  localStorage.setItem('theme', theme);
+}
 
 // Animate Theme Change
 const showThemeSlide = (parent) => {
@@ -37,8 +44,10 @@ gear.parentNode.querySelectorAll('span').forEach((span) => {
 // Theme color changing
 primary_color.addEventListener('click', () => {
   body_class.classList = 'theme-1';
+  set_theme_store('theme-1');
 });
 
 secondary_color.addEventListener('click', () => {
   body_class.classList = 'theme-2';
+  set_theme_store('theme-2');
 });
